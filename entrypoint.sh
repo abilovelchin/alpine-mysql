eb05233befb3fa6a2ba4a352412710e71c0b1e29
Elchin Abilov <abilovelchin@gmail.com>
2026-03-12 22:31:49 +0400

    Create entrypoint.sh for MySQL setup
    Add entrypoint script for MySQL initialization and startup

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Məlumat bazası yaradılır..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+
+    cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "${MYSQL_ROOT_PASSWORD}" WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO "${MYSQL_USER}"@'%' IDENTIFIED BY "${MYSQL_PASSWORD}";
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+FLUSH PRIVILEGES;
+EOF
+
+    /usr/bin/mysqld --user=mysql --bootstrap < $tfile
+    rm -f $tfile
+fi
+
+echo "MySQL başladılır..."
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --address=0.0.0.0
